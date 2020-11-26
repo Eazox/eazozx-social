0675a29873a44c802f3bb645967e3583aa546e5c
Onuoha Collins <onuohacollinschidubem@gmail.com>
2020-11-26 12:24:45 -0800

    new changes

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView, Text, View,  StyleSheet} from 'react-native';
 import tailwind from 'tailwind-rn';
 
 const App = () => {
@@ -7,11 +7,24 @@ const App = () => {
     <SafeAreaView style={tailwind('flex-1 items-center justify-center')}>
     <View style={tailwind('bg-blue-500 px-5 py-3 rounded-full')}>
       <Text style={tailwind('text-white font-semibold text-lg')}>
-        Hello Teams 👋
-      </Text>
+        Hello  collins welcome to react native 👋 
+      </Text>  
+
+        <Text style={Styles.textColor}>hello wellcome to react native</Text>
     </View>
   </SafeAreaView>
   );
 }
+
+const Styles = StyleSheet.create({
+   textColor: {
+     color: "blue",
+     alignItems: "center",
+     justifyContent: "center",
+     marginLeft: 50
+     
+   },
+})
+
 
 export default App;
