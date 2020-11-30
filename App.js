967a1a318df7fe01fee3a7323ed4d3490db69856
engreric <okemmadueric@gmail.com>
2020-11-30 13:34:04 +0100

    [fix] splash screen

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { SafeAreaView, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native'
 import tailwind from 'tailwind-rn'
 import SplashScreen from './src/components/splashScreen/SplashScreen'
 
 const App = () => {
   return (
-    <SafeAreaView style={tailwind('flex-1 items-center justify-center')}>
+    <SafeAreaView style={tailwind('flex-1')}>
       <SplashScreen />
     </SafeAreaView>
   )
--- src/components/splashScreen/SplashScreen.js
+++ src/components/splashScreen/SplashScreen.js
@@ -1,22 +1,47 @@
-import React from 'react'
-import { View, Image, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, Image, StyleSheet, Dimensions } from 'react-native'
+
+import * as Progress from 'react-native-progress'
 import Logo from '../../images/logo.png'
 
+const { height, width } = Dimensions.get('window')
+
 const SplashScreen = () => {
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    let count = 0
+    if (count !== 1) {
+      count++
+      setTimeout(() => {
+        setCounter(count + 0.1)
+      }, 3000)
+    }
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Image source={Logo} style={styles.logo} />
+    <View>
+      <View style={styles.container}>
+        <Image source={Logo} style={styles.logo} />
+      </View>
+
+      <Progress.Bar
+        style={{ alignSelf: 'center', bottom: 20 }}
+        progress={counter}
+        animated
+        height={10}
+        borderColor='gray'
+        color='rgba(87,141,222,1)'
+        width={width - 5}
+      />
     </View>
   )
 }
-
+console.log(Dimensions.get('window').height - 300)
 const styles = StyleSheet.create({
-  logo: {
-    marginLeft: 12
-  },
-
   container: {
-    flex: 1,
+    height,
+    // backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center'
   }
