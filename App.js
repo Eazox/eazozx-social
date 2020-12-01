0e9a049fc9c46d83935644fc57ae4b9f8207cc03
okoli-ryan50 <okoliryan50@gmail.com>
2020-12-01 17:40:42 +0100

    created Signup UI

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
-import React from 'react'
-import { SafeAreaView, Text, View } from 'react-native'
-import tailwind from 'tailwind-rn'
-import Sample from './src/Sample'
-import SplashScreen from './src/components/SplashScreen'
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import tailwind from 'tailwind-rn';
+import Sample from './src/Sample';
+import SplashScreen from './src/components/main components/SplashScreen';
+import Signup from './src/components/main components/Signup'
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
+import { AppLoading } from 'expo';
 
 const App = () => {
-  return (
-    <SafeAreaView style={tailwind('flex-1 items-center justify-center')}>
-      <SplashScreen/>
-    </SafeAreaView>
-  )
-}
+	let [fontsLoaded] = useFonts({
+		Roboto: require('native-base/Fonts/Roboto.ttf'),
+		Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+		Gilroy: require('./assets/fonts/Gilroy-Light.otf'),
+		...Ionicons.font,
+	});
 
-export default App
+	if (!fontsLoaded) return <AppLoading />;
+	else
+		return (
+			<SafeAreaView style={tailwind('flex-1 items-center justify-center')}>
+				{/* <SplashScreen /> */}
+        <Signup/>
+			</SafeAreaView>
+		);
+};
+
+export default App;
