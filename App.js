65323f55cf103bdaab977b9dc4f8729a57cfb410
Onuoha Collins <onuohacollinschidubem@gmail.com>
2020-12-01 22:27:00 -0800

    new changes

--- App.js
+++ App.js
@@ -2,34 +2,22 @@ import React from 'react'
 import { SafeAreaView, Text, View, StyleSheet } from 'react-native'
 import tailwind from 'tailwind-rn'
 import Sample from './src/Sample'
+import LoginScreen from './src/Screens/LoginScreen'
 
 
 const App = () => {
   return (
-    <SafeAreaView style={tailwind('flex-1 items-center justify-center')}>
-    <View style={tailwind('bg-blue-500 px-5 py-3 rounded-full')}>
-      <Text style={tailwind('text-white font-semibold text-lg')}>
-        Hello  collins welcome to react nati👋 
-      </Text>  
-        
-      <Sample />
-      <View style={tailwind('bg-blue-500 px-5 py-3 rounded-full')}>
-        <Text style={tailwind('text-white font-semibold text-lg')}> Here you go 👋</Text>
-      </View>
-      
+    <SafeAreaView>
+      <View>
+      <LoginScreen />
     </View>
   </SafeAreaView>
   );
 }
 
 const Styles = StyleSheet.create({
-   textColor: {
-     color: "blue",
-     alignItems: "center",
-     justifyContent: "center",
-     marginLeft: 50
+   
      
-   },
 })
 
 
