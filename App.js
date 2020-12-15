06e42747a39d09c46b58562d42dda2c03994d662
engreric <okemmadueric@gmail.com>
2020-12-15 09:44:20 +0100

    code cleanup

--- App.js
+++ App.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
-import tailwind from 'tailwind-rn';
-import Sample from './src/Sample';
-import SplashScreen from './src/components/main components/SplashScreen';
+import React from 'react'
+import { SafeAreaView, Text, View } from 'react-native'
+import tailwind from 'tailwind-rn'
+import Sample from './src/Sample'
+import SplashScreen from './src/components/main components/SplashScreen'
 // import Signup from './src/components/main components/Signup'
-import Verification from './src/components/main components/Verification';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
-import { useFonts } from 'expo-font';
-import { AppLoading } from 'expo';
+import Verification from './src/components/main components/Verification'
+import * as Font from 'expo-font'
+import { Ionicons } from '@expo/vector-icons'
+import { useFonts } from 'expo-font'
+import { AppLoading } from 'expo'
 
 const App = () => {
-	let [fontsLoaded] = useFonts({
-		Roboto: require('native-base/Fonts/Roboto.ttf'),
-		Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-		Gilroy: require('./assets/fonts/Gilroy-Light.otf'),
-		...Ionicons.font,
-	});
+  const [fontsLoaded] = useFonts({
+    Roboto: require('native-base/Fonts/Roboto.ttf'),
+    Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+    Gilroy: require('./assets/fonts/Gilroy-Light.otf'),
+    ...Ionicons.font
+  })
 
-	if (!fontsLoaded) return <AppLoading />;
-	else
-		return (
-			<SafeAreaView style={tailwind('flex-1 items-center justify-center')}>
-				{/* <SplashScreen /> */}
-				{/* <Signup /> */}
-				<Verification/>
-			</SafeAreaView>
-		);
-};
+  return !fontsLoaded ? (
+    <AppLoading />
+  ) : (
+    <SafeAreaView style={tailwind('flex-1 items-center justify-center')}>
+      {/* <SplashScreen /> */}
+      {/* <Signup /> */}
+      <Verification />
+    </SafeAreaView>
+  )
+}
 
-export default App;
+export default App
