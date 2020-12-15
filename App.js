30bcbb9953f0fce0cfb342c9483223b0ad2111cf
Onuoha Collins <onuohacollinschidubem@gmail.com>
2020-12-15 14:39:44 -0800

    new chages

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import React from 'react'
+
 import { SafeAreaView, Text, View, StyleSheet } from 'react-native'
 import tailwind from 'tailwind-rn'
 import SplashScreen from './src/components/splashScreen/SplashScreen'
+import { Ionicons, MaterialIcons } from '@expo/vector-icons'
+import { useFonts } from 'expo-font'
+import { AppLoading } from 'expo'
+import { NavigationContainer } from '@react-navigation/native'
+import { Theme } from './src/Constants'
+import DrawerNavigation from './src/navigations/DrawerNavigation'
+
+
 
 
 const App = () => {
+
   return (
-    <SafeAreaView style={tailwind('flex-1')}>
+    <SafeAreaView >
       <SplashScreen />
     </SafeAreaView>
   )
@@ -25,5 +35,6 @@ const Styles = StyleSheet.create({
 
 
 
-export default App
+  
 
+export default App
