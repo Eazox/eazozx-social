59d8c6b926d96dadc9f1be963ce0423466b52186
Onuoha Collins <onuohacollinschidubem@gmail.com>
2021-01-25 13:46:33 -0800

    new changes

--- src/components/mainComponents/login/LoginScreen.js
+++ src/components/mainComponents/login/LoginScreen.js
@@ -5,7 +5,6 @@ import Login from '../../../../src/images/login.png'
 import facebook from '../../../images/Facebook.png'
 import googleLogo from '../../../images/Google.png'
 import Header from '../../subComponents/Header'
-// import {CustomPassword } from '../../subComponents/CustomFontComponents'
 
 class LoginScreen extends Component {
   constructor(props) {
