07b23045e4afff2441413a60a053bac5bda3a04f
okoli-ryan50 <mariaunawu@gmail.com>
2021-02-02 23:40:28 +0100

    fix app.js }

--- App.js
+++ App.js
@@ -1,33 +1,14 @@
 import React from 'react'
-import { SafeAreaView, Text, View, StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native'
 
 import ForgotPassword from './src/components/mainComponents/forgotPassword/ForgotPassword'
-import LoginScreen from './src/components/mainComponents/login/LoginScreen'
-
-
 
 const App = () => {
-
   return (
-    <SafeAreaView >
-
+    <SafeAreaView>
       <ForgotPassword />
     </SafeAreaView>
   )
-
-  }
-
-
-const Styles = StyleSheet.create({
-   textColor: {
-     color: "blue",
-     alignItems: "center",
-     justifyContent: "center",
-     marginLeft: 50
-     
-   },
-})
-
 }
 
 export default App
