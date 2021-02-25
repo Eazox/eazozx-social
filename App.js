f515fec9851ddf1a84e71ba85908179e9d630db4
Onuoha Collins <onuohacollinschidubem@gmail.com>
2021-02-25 10:40:32 +0100

    new changes

--- App.js
+++ App.js
@@ -10,29 +10,8 @@ import { Theme } from './src/Constants'
 // import DrawerNavigation from './src/navigations/DrawerNavigation'
 import CurrentStack from './src/navigations/CurrentStack'
 
-// const store = configureStore()
 
 const App = () => {
-  const [fontsLoaded] = useFonts({
-    Roboto: require('native-base/Fonts/Roboto.ttf'),
-    Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-    Gilroy: require('./assets/fonts/Gilroy-Light.otf'),
-    Gilroy_bold: require('./assets/fonts/Gilroy-Bold.ttf'),
-    Gilroy_heavy: require('./assets/fonts/Gilroy-Heavy.ttf'),
-    Gilroy_medium: require('./assets/fonts/Gilroy-Medium.ttf'),
-    Gilroy_regular: require('./assets/fonts/Gilroy-Regular.ttf'),
-    Gilroy_semibold: require('./assets/fonts/Gilroy-SemiBold.ttf'),
-    Gilroy_thin: require('./assets/fonts/Gilroy-Thin.ttf'),
-    Gilroy_ultralight: require('./assets/fonts/Gilroy-UltraLight.ttf'),
-    ...Ionicons.font,
-    ...MaterialIcons.font,
-    ...Entypo.font,
-    ...MaterialCommunityIcons.font
-  })
-
-  if (!fontsLoaded) {
-    return <AppLoading />
-  } else {
     return (
       <Provider store={store}>
         <NavigationContainer theme={Theme}>
